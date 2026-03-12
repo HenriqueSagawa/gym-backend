7a0cdedf1b8be540a9a7c8f6010c8c87725a52e2
Henrique Sagawa <henriquetutomusagawa@gmail.com>
2026-03-12 16:57:07 -0300

    feat(user): Add API routes for authentication and user management.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
 import { authenticate, authorize } from "../middlewares/auth.middleware";
+import { UserController } from "../controllers/user.controller";
 
 const router = Router();
 const auth = new AuthController();
+const user = new UserController();
 
 router.post("/auth/login", auth.loginUser.bind(auth));
 router.post("/auth/login/professional", auth.loginProfessional.bind(auth));
 router.post("/auth/login/admin", auth.loginGymAdmin.bind(auth));
 router.post("/auth/change-password", authenticate, auth.changePassword.bind(auth));
 router.get("/auth/me", authenticate, auth.me.bind(auth));
+
+router.post('/gyms/:gymId/register', user.register.bind(user));
+router.post('/users', authenticate, authorize('GYM_ADMIN'), user.create.bind(user));
+router.get('/users', authenticate, authorize('GYM_ADMIN', 'PROFESSIONAL'), user.list.bind(user));
+router.get('/users/me', authenticate, authorize('USER'), user.getMe.bind(user));
+router.get('/users/:id', authenticate, authorize('GYM_ADMIN', 'PROFESSIONAL'), user.getById.bind(user));
+router.put('/users/me', authenticate, authorize('USER'), user.updateProfile.bind(user));
+router.put('/users/:id', authenticate, authorize('GYM_ADMIN'), user.updateProfile.bind(user));
+router.delete('/users/:id', authenticate, authorize('GYM_ADMIN'), user.deactivate.bind(user));
 
 export default router;
